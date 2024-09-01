9dcd5ca839d23c945987cd8fad7a94349d61795f
danya <danildb2109@gmail.com>
2024-09-01 18:20:35 +0300

    user button upgrade

--- src/widgets/Header/ui/user-button.tsx
+++ src/widgets/Header/ui/user-button.tsx
@@ -1,4 +1,7 @@
 import { FaUser } from 'react-icons/fa';
+
+import { logout } from '@/shared/api';
+import { dmSans } from '@/shared/assets/fonts';
 import { useCurrentUser } from '@/shared/model';
 import { RoundedButton } from '@/shared/ui/buttons';
 import {
@@ -10,7 +13,6 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/shared/ui/shadcn';
-import { logout } from '@/shared/api';
 
 export const UserButton = () => {
   const user = useCurrentUser();
@@ -26,8 +28,28 @@ export const UserButton = () => {
         </Avatar>
       </DropdownMenuTrigger>
       <DropdownMenuContent>
-        <DropdownMenuItem>
-          <RoundedButton text={'Logout'} onClick={() => logout()} />
+        <div className='flex gap-4 items-center'>
+          <Avatar>
+            <AvatarImage src={user?.image || ''} />
+            <AvatarFallback className='bg-black'>
+              <FaUser className='text-white' />
+            </AvatarFallback>
+          </Avatar>
+          <div className='flex flex-col items-start'>
+            <h5 className={`${dmSans.className} text-black font-medium`}>
+              {user?.name}
+            </h5>
+            <h5 className={`${dmSans.className} text-dark-gray`}>
+              {user?.email}
+            </h5>
+          </div>
+        </div>
+        <DropdownMenuItem className='mt-8'>
+          <RoundedButton
+            text={'Logout'}
+            className='w-full'
+            onClick={() => logout()}
+          />
         </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
